3f65b23187b501b5e84812bf46aef2cc36d6bf72
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-10-25 10:56:43 +0900

    fixed library git

---
+++ frontend/lib/withData.js
@@ -0,0 +1,24 @@
+import ApolloClient from 'apollo-boost';
+import withApollo from 'next-with-apollo';
+import { endpoint, prodEndpoint } from '../config';
+
+function createClient({ headers }) {
+	// Set up apollo clietn
+	return new ApolloClient({
+		uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+		request: operation => {
+			operation.setContext({
+				fetchOptions: {
+					credentials: 'include'
+				},
+				headers
+			});
+		},
+		// Local Data
+		clientState: {
+			resolvers: {}
+		}
+	});
+}
+
+export default withApollo(createClient);
