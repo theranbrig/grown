8bc1775e6d5c0765f86653094dd97965e4ede890
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-11-03 13:23:33 +0900

    updated config file

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,4 @@
-// This is client side config only - don't put anything in here that shouldn't be public!
+
 export const endpoint = `http://localhost:4444`;
-export const perPage = 6;
 export const prodEndpoint = 'https://grown-yoga-prod.herokuapp.com/';
-***REMOVED***
-***REMOVED***
+export const perPage = 6;
