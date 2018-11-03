2ef059ccce29fd8cb90092097241fb74df483bc5
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-11-03 11:54:03 +0900

    updated url

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,6 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
 export const perPage = 6;
-export const prodEndpoint =
-	'https://grown-production-application.herokuapp.com/grown-production/prod';
+export const prodEndpoint = 'https://grown-production-application.herokuapp.com/';
 ***REMOVED***
 ***REMOVED***
