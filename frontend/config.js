27db4a6c93ddbcacdeba1d6c9f34325c22d719f6
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-11-03 18:23:06 +0900

    updated file structure

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,5 @@
 export const endpoint = `http://localhost:4444`;
 export const prodEndpoint = 'https://backend-accerlcqyl.now.sh/';
 export const perPage = 6;
+export const GOOGLE_MAPS_API_KEY = ***REMOVED***;
+export const GOOGLE_GEOCODE_API_KEY = ***REMOVED***;
