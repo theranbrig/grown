9ea29f14cdef39fc8ef322eccbdda99374746eb6
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-11-03 13:23:33 +0900

    updated config file

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,4 @@
-// This is client side config only - don't put anything in here that shouldn't be public!
+
 export const endpoint = `http://localhost:4444`;
-export const perPage = 6;
 export const prodEndpoint = 'https://grown-yoga-prod.herokuapp.com/';
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyBIv-cX5AWx2UUXLpfs9Ls4r9xKubrZe-Q';
-export const GOOGLE_GEOCODE_API_KEY = 'AIzaSyBLYQHBA8GVgQTyBcCEnmDAAt4mBbfa3kw';
+export const perPage = 6;
