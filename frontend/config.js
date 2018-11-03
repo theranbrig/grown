23ab30116165f2794e098581a472253dc1b00745
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-11-03 16:08:35 +0900

    changed to now

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,3 @@
-
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = 'https://grown-yoga-prod.herokuapp.com/';
+export const prodEndpoint = 'https://grown-backend-rjgbfjpplb.now.sh/';
 export const perPage = 6;
