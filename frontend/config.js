272d9508f28bc9f0bf7e5b936eb864869a0cf5ba
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-11-05 22:11:38 +0900

    updated for production

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = 'https://backend-accerlcqyl.now.sh/';
+export const prodEndpoint = 'https://grownfarms-yoga.herokuapp.com/';
 export const perPage = 6;
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyBIv-cX5AWx2UUXLpfs9Ls4r9xKubrZe-Q';
 export const GOOGLE_GEOCODE_API_KEY = 'AIzaSyBLYQHBA8GVgQTyBcCEnmDAAt4mBbfa3kw';
