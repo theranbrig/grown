5d95ee04b70bdf58a87ee2407603d9f2b6e86fa4
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-11-05 23:10:17 +0900

    updated login

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,28 +8,26 @@ const server = createServer();
 
 server.express.use(cookieParser());
 
-// Express middleware to handle cookies (JWT)
-
-// server.express.use((req, res, next) => {
-// 	const { token } = req.cookies;
-// 	if (token) {
-// 		const { userId } = jwt.verify(token, process.env.APP_SECRET);
-// 		req.userId = userId;
-// 	}
-// 	next();
-// });
-
-// Express middleware to populate current user
+server.express.use((req, res, next) => {
+	const { token } = req.cookies;
+	if (token) {
+		const { userId } = jwt.verify(token, process.env.APP_SECRET);
+		req.userId = userId;
+	}
+	next();
+});
 
 server.express.use(async (req, res, next) => {
 	if (!req.userId) return next();
-	const user = await db.query.user({ where: { id: req.userId } }, '{id, permissions, email, name}');
+	const user = await db.query.user(
+		{ where: { id: req.userId } },
+		'{ id, permissions, email, name }'
+	);
 	req.user = user;
 	next();
 });
 
 server.start(
-	//For Production Right Now.  Needs to be fixed later.
 	{
 		cors: {
 			credentials: true,
@@ -40,5 +38,3 @@ server.start(
 		console.log(`Server is now running on port http://localhost:${deets.port}`);
 	}
 );
-
-// Access to fetch at 'https://grown-yoga-prod.herokuapp.com/' from origin 'https://grown-next.herokuapp.com' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: The 'Access-Control-Allow-Origin' header has a value 'https://grown-next.herokuapp.com/' that is not equal to the supplied origin. Have the server send the header with a valid value, or, if an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
