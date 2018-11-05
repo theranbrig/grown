560e58c97642a5521217b7274897f3fd167ab7c1
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-11-06 01:24:13 +0900

    cookie error fix

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,10 +1,12 @@
 import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import withApollo from 'next-with-apollo';
 import { endpoint, prodEndpoint } from '../config';
 
 function createClient({ headers }) {
 	// Set up apollo client
 	return new ApolloClient({
+		cache: new InMemoryCache(),
 		uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
 		request: async operation => {
 			operation.setContext({
