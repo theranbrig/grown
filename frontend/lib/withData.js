b04bc3afe00ee3b3cd9410a1b92ca2daaaca9cfe
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-11-06 11:25:26 +0900

    updated deploy

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,22 +1,22 @@
-import ApolloClient from 'apollo-boost';
+import ApolloBoostClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import withApollo from 'next-with-apollo';
 import { endpoint, prodEndpoint } from '../config';
 
 function createClient({ headers }) {
 	// Set up apollo client
-	return new ApolloClient({
+	return new ApolloBoostClient({
 		cache: new InMemoryCache(),
 		uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
 		credentials: 'include',
-		// request: operation => {
-		// 	operation.setContext({
-		// 		fetchOptions: {
-		// 			credentials: 'include'
-		// 		},
-		// 		headers
-		// 	});
-		// },
+		request: operation => {
+			operation.setContext({
+				fetchOptions: {
+					credentials: 'include'
+				},
+				headers
+			});
+		},
 		// Local Data
 		clientState: {
 			resolvers: {}
