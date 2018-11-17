d7a784cf03706974391d3b58ad46f6e4c3060d85
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-11-17 11:24:11 +0900

    updated headers

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,10 +1,9 @@
-import ApolloClient from 'apollo-boost';
 import withApollo from 'next-with-apollo';
+import ApolloClient from 'apollo-boost';
 import { endpoint, prodEndpoint } from '../config';
 import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
-	// Set up apollo client
 	return new ApolloClient({
 		uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
 		request: operation => {
@@ -12,15 +11,15 @@ function createClient({ headers }) {
 				fetchOptions: {
 					credentials: 'include'
 				},
-				...headers
+				headers
 			});
 		},
-		// Local Data
+		// local data
 		clientState: {
 			resolvers: {
 				Mutation: {
 					toggleCart(_, variables, { cache }) {
-						// Read the cartOpen value from the cache
+						// read the cartOpen value from the cache
 						const { cartOpen } = cache.readQuery({
 							query: LOCAL_STATE_QUERY
 						});
