54a2598d21ce02021257d6f13a6cde422e825d34
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-12-16 18:18:36 +0900

    updated with eslint and proptypes

--- frontend/pages/join.js
+++ frontend/pages/join.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import SignUp from '../components/SignUp';
 
-const UserPage = props => (
-	<div>
-		<SignUp />
-	</div>
+const UserPage = () => (
+  <div>
+    <SignUp />
+  </div>
 );
 
 export default UserPage;
--- frontend/pages/resetpassword.js
+++ frontend/pages/resetpassword.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PasswordResetRequest from '../components/PasswordResetRequest';
 
-const PasswordResetPage = props => (
-	<div>
-		<PasswordResetRequest />
-	</div>
+const PasswordResetPage = () => (
+  <div>
+    <PasswordResetRequest />
+  </div>
 );
 
 export default PasswordResetPage;
